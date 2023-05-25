5a7698bc2933e3edfd8176b1f5f77a75abec4e6c
quincy <quincy_sky@163.com>
2023-05-25 21:19:13 +0800

    complete "client"

---
+++ core/src/main/java/quincy/remoting/transport/netty/client/NettyRpcClient.java
@@ -0,0 +1,122 @@
+package quincy.remoting.transport.netty.client;
+
+import enums.CompressTypeEnum;
+import enums.SerializationTypeEnum;
+import enums.ServiceDiscoveryEnum;
+import extension.ExtensionLoader;
+import factory.SingletonFactory;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import quincy.registry.ServiceDiscovery;
+import quincy.remoting.constants.RpcConstants;
+import quincy.remoting.dto.RpcMessage;
+import quincy.remoting.dto.RpcRequest;
+import quincy.remoting.dto.RpcResponse;
+import quincy.remoting.transport.RpcRequestTransport;
+import quincy.remoting.transport.codec.RpcMessageDecoder;
+import quincy.remoting.transport.codec.RpcMessageEncoder;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author quincy
+ * @create 2023 - 05 - 22 21:27
+ */
+@Slf4j
+public class NettyRpcClient implements RpcRequestTransport {
+    private final ServiceDiscovery serviceDiscovery;
+    private final UnprocessedRequests unprocessedRequests;
+    private final ChannelProvider channelProvider;
+    private final Bootstrap bootstrap;
+    private final EventLoopGroup eventLoopGroup;
+
+    public NettyRpcClient() {
+
+        eventLoopGroup = new NioEventLoopGroup();
+        bootstrap =  new Bootstrap();
+
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        ChannelPipeline p = socketChannel.pipeline();
+                        // 如果在15秒内没有消息发送，将发送一个heartbeat请求
+                        p.addLast(new IdleStateHandler(0, 5, 0, TimeUnit.SECONDS));
+                        p.addLast(new RpcMessageEncoder());
+                        p.addLast(new RpcMessageDecoder());
+                        p.addLast(new NettyRpcClientHandler());
+                    }
+                });
+
+        this.serviceDiscovery = ExtensionLoader.getExtensionLoader(ServiceDiscovery.class).getExtension(ServiceDiscoveryEnum.ZK.getName());
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+        channelProvider = SingletonFactory.getInstance(ChannelProvider.class);
+    }
+
+
+    @SneakyThrows
+    public Channel doConnect(InetSocketAddress inetSocketAddress) {
+        CompletableFuture<Channel> completableFuture = new CompletableFuture<>();
+        bootstrap.connect(inetSocketAddress).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                log.info("Then client has connected [{}] successfully!", inetSocketAddress.toString());
+                completableFuture.complete(future.channel());
+            } else {
+                throw new IllegalStateException();
+            }
+        });
+        return completableFuture.get();
+    }
+
+    @Override
+    public Object sendRpcRequest(RpcRequest rpcRequest) {
+        CompletableFuture<RpcResponse<Object>> resultFuture = new CompletableFuture<>();
+        InetSocketAddress inetSocketAddress = serviceDiscovery.lookupService(rpcRequest);
+        Channel channel = getChannel(inetSocketAddress);
+        if (channel.isActive()) {
+            unprocessedRequests.put(rpcRequest.getRequestId(), resultFuture);
+            RpcMessage rpcMessage = RpcMessage.builder().data(rpcRequest)
+                    .codec(SerializationTypeEnum.KYRO.getCode())
+                    .compress(CompressTypeEnum.GZIP.getCode())
+                    .messageType(RpcConstants.REQUEST_TYPE).build();
+            channel.writeAndFlush(rpcMessage).addListener((ChannelFutureListener) future ->{
+                if (future.isSuccess()) {
+                    log.info("client send message: [{}]", rpcMessage);
+                } else {
+                    future.channel().close();
+                    resultFuture.completeExceptionally(future.cause());
+                    log.error("Send failed:", future.cause());
+                }
+            });
+        } else {
+            throw new IllegalStateException();
+        }
+        return resultFuture;
+    }
+
+    public Channel getChannel(InetSocketAddress inetSocketAddress) {
+        Channel channel = channelProvider.get(inetSocketAddress);
+        if (channel == null) {
+            channel = doConnect(inetSocketAddress);
+            channelProvider.set(inetSocketAddress, channel);
+        }
+        return channel;
+    }
+
+    public void close() {
+        eventLoopGroup.shutdownGracefully();
+    }
+}
---
+++ core/src/main/java/quincy/remoting/transport/netty/client/NettyRpcClientHandler.java
@@ -0,0 +1,83 @@
+package quincy.remoting.transport.netty.client;
+
+import enums.CompressTypeEnum;
+import enums.SerializationTypeEnum;
+import factory.SingletonFactory;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.ReferenceCountUtil;
+import lombok.extern.slf4j.Slf4j;
+import quincy.remoting.constants.RpcConstants;
+import quincy.remoting.dto.RpcMessage;
+import quincy.remoting.dto.RpcResponse;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 自定义客户端处理器来发送数据到服务端
+ * <p>
+ *     如果继承自SimpleChannelInboundHandler的话就不需要考虑ByteBuf的释放，内部的channelRead()方法会替你释放掉ByteBuf,避免可能导致的内存泄漏问题
+ * </p>
+ *
+ * @author quincy
+ * @create 2023 - 05 - 22 21:23
+ */
+@Slf4j
+public class NettyRpcClientHandler extends ChannelInboundHandlerAdapter {
+    private final UnprocessedRequests unprocessedRequests;
+    private final NettyRpcClient nettyRpcClient;
+
+    public NettyRpcClientHandler() {
+        this.unprocessedRequests = SingletonFactory.getInstance(UnprocessedRequests.class);
+        this.nettyRpcClient = SingletonFactory.getInstance(NettyRpcClient.class);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        try {
+            log.info("client receive msg: [{}]", msg);
+            if (msg instanceof RpcMessage) {
+                RpcMessage tmp = (RpcMessage) msg;
+                byte messageType = tmp.getMessageType();
+                if (messageType == RpcConstants.HEARTBEAT_RESPONSE_TYPE) {
+                    log.info("heart [{}]]", tmp.getData());
+                } else if (messageType == RpcConstants.RESPONSE_TYPE) {
+                    RpcResponse<Object> rpcResponse = (RpcResponse<Object>) tmp.getData();
+                    unprocessedRequests.complete(rpcResponse);
+                }
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleState state = ((IdleStateEvent) evt).state();
+            if (state == IdleState.WRITER_IDLE) {
+                log.info("write idle happen [{}]", ctx.channel().remoteAddress());
+                RpcMessage rpcMessage = new RpcMessage();
+                Channel channel = nettyRpcClient.getChannel((InetSocketAddress) ctx.channel().remoteAddress());
+                rpcMessage.setCodec(SerializationTypeEnum.KYRO.getCode());
+                rpcMessage.setCompress(CompressTypeEnum.GZIP.getCode());
+                rpcMessage.setMessageType(RpcConstants.HEARTBEAT_REQUEST_TYPE);
+                rpcMessage.setData(RpcConstants.PING);
+                channel.writeAndFlush(rpcMessage).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error("client catch exception:", cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
